da65d5275f08d009836fc6a563306c5c7c5c1436
akilchomoko <akil@chomoko.com>
2021-04-21 16:10:15 +0100

    New plant form added

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,43 @@
 class PlantsController < ApplicationController
+  def index
+    @plants = Plant.all
+  end
+
+  def show
+    @plant = Plant.find(params[:id])
+  end
+
+  def new
+    @plant = Plant.new
+  end
+
+  def create
+    @plant = Plant.new(params_plant)
+    @plant.user = current_user
+    if @plant.save
+      redirect_to plants_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @plant = Plant.find(params[:id])
+  end
+
+  def update
+    @plant.update(params_plant)
+    @plant.save
+  end
+
+  def destroy
+    @plant.delete
+    redirect_to plants_path
+  end
+
+  private
+
+  def params_plant
+    params.require(:plant).permit(:name)
+  end
 end
